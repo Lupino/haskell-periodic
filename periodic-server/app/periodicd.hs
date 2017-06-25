4be28487b6fd36646a8f50d20bb1d79f61b92f08
Lupino <lmjubuntu@gmail.com>
2017-06-25 22:18:44 +0800

    periodicd argument

--- periodic-server/app/periodicd.hs
+++ periodic-server/app/periodicd.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Main
   (
     main
@@ -6,22 +8,38 @@ module Main
 import           Periodic.Server
 import           System.Environment (getArgs)
 
-defaultPort :: String
-defaultPort = "5000"
+data Options = Options { oHost    :: Maybe String
+                       , oPort    :: String
+                       , oPath    :: String
+                       , showHelp :: Bool
+                       }
 
-defaultStorePath :: String
-defaultStorePath = "state"
+defaultOptions :: Options
+defaultOptions = Options { oHost    = Nothing
+                         , oPort    = "5000"
+                         , oPath    = "state"
+                         , showHelp = False
+                         }
+
+parseOptions :: [String] -> Options
+parseOptions []              = defaultOptions
+parseOptions ("-P":x:xs)     = (parseOptions xs) { oPort = x }
+parseOptions ("--port":x:xs) = (parseOptions xs) { oPort = x }
+parseOptions ("-H":x:xs)     = (parseOptions xs) { oHost = Just x }
+parseOptions ("--host":x:xs) = (parseOptions xs) { oHost = Just x }
+parseOptions ("-p":x:xs)     = (parseOptions xs) { oPath = x }
+parseOptions ("--path":x:xs) = (parseOptions xs) { oPath = x }
+parseOptions ("-h":xs)       = (parseOptions xs) { showHelp = True }
+parseOptions ("--help":xs)   = (parseOptions xs) { showHelp = True }
+parseOptions (x:xs)          = parseOptions xs
+
+printHelp :: IO ()
+printHelp = do
+  putStrLn "periodicd [--port|-P 5000] [--host|-H 0.0.0.0] [--path|-p state] [--help|-h]"
 
 main :: IO ()
 main = do
-  args <- getArgs
+  (Options {..}) <- parseOptions <$> getArgs
 
-  let (path, host, port) = case args of
-                             [x]    -> (defaultStorePath, Nothing, x)
-                             [x, y] -> (defaultStorePath, Just x, y)
-                             [x, y, z] -> (x, Just y, z)
-                             _      -> (defaultStorePath, Nothing, defaultPort)
-
-
-  startServer path host port
-
+  if showHelp then printHelp
+              else startServer oPath oHost oPort
