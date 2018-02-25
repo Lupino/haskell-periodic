cb3bbc1582eb7dcade82bb52fd861c65934ef7ac
Lupino <lmjubuntu@gmail.com>
2018-02-25 15:44:46 +0800

    use binary header

--- periodic-common/src/Periodic/Utils.hs
+++ periodic-common/src/Periodic/Utils.hs
@@ -1,33 +1,17 @@
 module Periodic.Utils
   (
-    makeHeader
-  , parseHeader
-  , maxLength
+    maxLength
   , tryIO
   , getEpochTime
   ) where
 
-import           Data.Bits             (shiftL, shiftR, (.&.), (.|.))
-import qualified Data.ByteString.Char8 as B
+import           Control.Exception (IOException, catch)
 
-import           Control.Exception     (IOException, catch)
-
-import           Data.Int              (Int64)
-import           Data.UnixTime         (getUnixTime, toEpochTime)
-
-makeHeader :: Int -> B.ByteString
-makeHeader x = c 24 `B.cons` c 16 `B.cons` c 8 `B.cons` c 0 `B.cons` B.empty
-  where c :: Int -> Char
-        c i = toEnum $ x `shiftR` i .&. 0xff
+import           Data.Int          (Int64)
+import           Data.UnixTime     (getUnixTime, toEpochTime)
 
 maxLength :: Int
 maxLength = 0x7fffffff
-
-parseHeader :: B.ByteString -> Int
-parseHeader = go [24, 16, 8, 0]
-  where go :: [Int] -> B.ByteString -> Int
-        go [] _     = 0
-        go (x:xs) h = fromEnum (B.head h) `shiftL` x .|. go xs (B.tail h)
 
 tryIO :: IO a -> IO (Either IOException a)
 tryIO m = catch (fmap Right m) (return . Left)
