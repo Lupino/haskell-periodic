8a22cb89b2d99460c0da140095a2dd669a37e8fb
Lupino <lmjubuntu@gmail.com>
2019-02-09 10:44:15 +0800

    periodic-client: periodic-run add lock support

--- periodic-client/app/periodic-run.hs
+++ periodic-client/app/periodic-run.hs
@@ -17,14 +17,14 @@ import qualified Data.Text                      as T (unpack)
 import           Data.Text.Encoding             (decodeUtf8With)
 import           Data.Text.Encoding.Error       (ignore)
 import           Periodic.Job                   (JobT, name, schedLater,
-                                                 workDone, workDone_, workFail,
-                                                 workload)
+                                                 withLock, workDone, workDone_,
+                                                 workFail, workload)
 import           Periodic.Socket                (getHost, getService)
 import           Periodic.Transport             (Transport)
 import           Periodic.Transport.TLS
 import qualified Periodic.Transport.WebSockets  as WS (makeClientTransport)
 import           Periodic.Transport.XOR         (makeXORTransport)
-import           Periodic.Types.Job             (FuncName (..))
+import           Periodic.Types                 (FuncName (..), LockName (..))
 import           Periodic.Worker                (addFunc, broadcast, runWorkerT,
                                                  work)
 import           System.Environment             (getArgs, lookupEnv)
@@ -44,6 +44,8 @@ data Options = Options
   , cert      :: FilePath
   , caStore   :: FilePath
   , thread    :: Int
+  , lockCount :: Int
+  , lockName  :: Maybe LockName
   , notify    :: Bool
   , useData   :: Bool
   , useStdout :: Bool
@@ -62,6 +64,8 @@ options t h f = Options
   , cert      = "client.pem"
   , caStore   = "ca.pem"
   , thread    = fromMaybe 1 t
+  , lockCount = 1
+  , lockName  = Nothing
   , notify    = False
   , useData   = False
   , useStdout = True
@@ -69,49 +73,53 @@ options t h f = Options
   , showHelp  = False
   }
 
-parseOptions :: [String] -> Options -> (Options, String, String, [String])
-parseOptions ("-H":x:xs)         opt = parseOptions xs opt { host      = x }
-parseOptions ("--host":x:xs)     opt = parseOptions xs opt { host      = x }
-parseOptions ("--xor":x:xs)      opt = parseOptions xs opt { xorFile   = x }
-parseOptions ("--tls":xs)        opt = parseOptions xs opt { useTls = True }
-parseOptions ("--ws":xs)         opt = parseOptions xs opt { useWs = True }
-parseOptions ("--hostname":x:xs) opt = parseOptions xs opt { hostName = x }
-parseOptions ("--cert-key":x:xs) opt = parseOptions xs opt { certKey = x }
-parseOptions ("--cert":x:xs)     opt = parseOptions xs opt { cert = x }
-parseOptions ("--ca":x:xs)       opt = parseOptions xs opt { caStore = x }
-parseOptions ("--thread":x:xs)   opt = parseOptions xs opt { thread = read x }
-parseOptions ("--help":xs)       opt = parseOptions xs opt { showHelp = True }
-parseOptions ("--broadcast":xs)  opt = parseOptions xs opt { notify = True }
-parseOptions ("--data":xs)       opt = parseOptions xs opt { useData = True }
-parseOptions ("--no-stdout":xs)  opt = parseOptions xs opt { useStdout = False }
-parseOptions ("--no-name":xs)    opt = parseOptions xs opt { useName = False }
-parseOptions ("-h":xs)           opt = parseOptions xs opt { showHelp = True }
-parseOptions []                  opt = (opt { showHelp = True }, "", "", [])
-parseOptions [_]                 opt = (opt { showHelp = True }, "", "", [])
-parseOptions (x:y:xs)            opt = (opt, x, y, xs)
+parseOptions :: [String] -> Options -> (Options, FuncName, String, [String])
+parseOptions ("-H":x:xs)           opt = parseOptions xs opt { host      = x }
+parseOptions ("--host":x:xs)       opt = parseOptions xs opt { host      = x }
+parseOptions ("--xor":x:xs)        opt = parseOptions xs opt { xorFile   = x }
+parseOptions ("--tls":xs)          opt = parseOptions xs opt { useTls = True }
+parseOptions ("--ws":xs)           opt = parseOptions xs opt { useWs = True }
+parseOptions ("--hostname":x:xs)   opt = parseOptions xs opt { hostName = x }
+parseOptions ("--cert-key":x:xs)   opt = parseOptions xs opt { certKey = x }
+parseOptions ("--cert":x:xs)       opt = parseOptions xs opt { cert = x }
+parseOptions ("--ca":x:xs)         opt = parseOptions xs opt { caStore = x }
+parseOptions ("--thread":x:xs)     opt = parseOptions xs opt { thread = read x }
+parseOptions ("--lock-count":x:xs) opt = parseOptions xs opt { lockCount = read x }
+parseOptions ("--lock-name":x:xs)  opt = parseOptions xs opt { lockName = Just (LockName $ B.pack x) }
+parseOptions ("--help":xs)         opt = parseOptions xs opt { showHelp = True }
+parseOptions ("--broadcast":xs)    opt = parseOptions xs opt { notify = True }
+parseOptions ("--data":xs)         opt = parseOptions xs opt { useData = True }
+parseOptions ("--no-stdout":xs)    opt = parseOptions xs opt { useStdout = False }
+parseOptions ("--no-name":xs)      opt = parseOptions xs opt { useName = False }
+parseOptions ("-h":xs)             opt = parseOptions xs opt { showHelp = True }
+parseOptions []                    opt = (opt { showHelp = True }, "", "", [])
+parseOptions [_]                   opt = (opt { showHelp = True }, "", "", [])
+parseOptions (x:y:xs)              opt = (opt, FuncName $ B.pack x, y, xs)
 
 printHelp :: IO ()
 printHelp = do
   putStrLn "periodic-run - Periodic task system worker"
   putStrLn ""
-  putStrLn "Usage: periodic-run [--host|-H HOST] [--xor FILE|--ws|--tls [--hostname HOSTNAME] [--cert-key FILE] [--cert FILE] [--ca FILE] [--broadcast] [--data] [--no-stdout] [--no-name]] funcname command [options]"
+  putStrLn "Usage: periodic-run [--host|-H HOST] [--xor FILE|--ws|--tls [--hostname HOSTNAME] [--cert-key FILE] [--cert FILE] [--ca FILE] [--thread THREAD] [--lock-name NAME] [--lock-count COUNT] [--broadcast] [--data] [--no-stdout] [--no-name]] funcname command [options]"
   putStrLn ""
   putStrLn "Available options:"
-  putStrLn "  -H --host      Socket path [$PERIODIC_PORT]"
-  putStrLn "                 eg: tcp://:5000 (optional: unix:///tmp/periodic.sock) "
-  putStrLn "     --xor       XOR Transport encode file [$XOR_FILE]"
-  putStrLn "     --tls       Use tls transport"
-  putStrLn "     --ws        Use websockets transport"
-  putStrLn "     --hostname  Host name"
-  putStrLn "     --cert-key  Private key associated"
-  putStrLn "     --cert      Public certificate (X.509 format)"
-  putStrLn "     --ca        trusted certificates"
-  putStrLn "     --thread    worker thread [$THREAD]"
-  putStrLn "     --broadcast is broadcast worker"
-  putStrLn "     --data      send work data to client"
-  putStrLn "     --no-stdout hidden the stdout"
-  putStrLn "     --no-name   ignore the job name"
-  putStrLn "  -h --help      Display help message"
+  putStrLn "  -H --host       Socket path [$PERIODIC_PORT]"
+  putStrLn "                  Eg: tcp://:5000 (optional: unix:///tmp/periodic.sock) "
+  putStrLn "     --xor        XOR Transport encode file [$XOR_FILE]"
+  putStrLn "     --tls        Use tls transport"
+  putStrLn "     --ws         Use websockets transport"
+  putStrLn "     --hostname   Host name"
+  putStrLn "     --cert-key   Private key associated"
+  putStrLn "     --cert       Public certificate (X.509 format)"
+  putStrLn "     --ca         Trusted certificates"
+  putStrLn "     --thread     Worker thread [$THREAD]"
+  putStrLn "     --lock-count Max lock count (optional: 1)"
+  putStrLn "     --lock-name  The lock name (optional: no lock)"
+  putStrLn "     --broadcast  Is broadcast worker"
+  putStrLn "     --data       Send work data to client"
+  putStrLn "     --no-stdout  Hidden the stdout"
+  putStrLn "     --no-name    Ignore the job name"
+  putStrLn "  -h --help       Display help message"
   putStrLn ""
   exitSuccess
 
@@ -130,9 +138,12 @@ main = do
     printHelp
 
   runWorkerT (makeTransport opts) host $ do
-    let proc = if notify then broadcast else addFunc
-
-    proc (FuncName $ B.pack func) $ processWorker opts cmd argv
+    let w = processWorker opts cmd argv
+    if notify then broadcast func w
+    else
+      case lockName of
+        Nothing -> addFunc func w
+        Just n  -> addFunc func $ withLock n lockCount w
     liftIO $ putStrLn "Worker started."
     work thread
 
