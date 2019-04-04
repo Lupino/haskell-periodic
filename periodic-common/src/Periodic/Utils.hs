58fbca03f2abf47e99525162d310cc550b6754ed
Lupino <lmjubuntu@gmail.com>
2019-04-04 13:47:34 +0800

    periodic-common: update getEpochTime

--- periodic-common/src/Periodic/Utils.hs
+++ periodic-common/src/Periodic/Utils.hs
@@ -4,15 +4,13 @@ module Periodic.Utils
   ) where
 
 import           Control.Exception (IOException, catch)
-import           Foreign.C.Types   (CTime (..))
-
-import           Data.Int          (Int64)
 import           Data.UnixTime     (getUnixTime, toEpochTime)
+import           Foreign.C.Types   (CTime (..))
 
 tryIO :: IO a -> IO (Either IOException a)
 tryIO m = catch (fmap Right m) (return . Left)
 
-getEpochTime :: IO Int64
+getEpochTime :: Num a => IO a
 getEpochTime = un . toEpochTime <$> getUnixTime
-  where un :: CTime -> Int64
-        un (CTime t) = t
+  where un :: Num a => CTime -> a
+        un (CTime t) = fromIntegral t
