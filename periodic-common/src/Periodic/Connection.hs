55b8875a0b06b71d5e00e78a8d88e69de5a670c0
Lupino <lmjubuntu@gmail.com>
2019-12-27 20:06:39 +0800

    update recvData 4m

--- periodic-common/src/Periodic/Connection.hs
+++ periodic-common/src/Periodic/Connection.hs
@@ -138,7 +138,7 @@ recv' ConnEnv{..} nbytes = do
   where readBuf :: Int -> IO B.ByteString
         readBuf 0  = return B.empty
         readBuf nb = do
-          buf <- recvData transport 1024
+          buf <- recvData transport 4194304
           when (B.null buf) $ throwIO TransportClosed
           if B.length buf >= nb then return buf
                                 else do
