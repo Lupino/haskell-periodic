71b268c68da94a40b2a013879ce649c2c3bcf734
Lupino <lmjubuntu@gmail.com>
2020-03-26 21:10:31 +0800

    update static build

--- release-static.nix
+++ release-static.nix
@@ -10,7 +10,7 @@ let
   # Pin static-haskell-nix version.
   static-haskell-nix =
       # Update this hash to use a different `static-haskell-nix` version:
-      fetchTarball https://github.com/nh2/static-haskell-nix/archive/b402b38c3af2300e71caeebe51b5e4e1ae2e924c.tar.gz;
+      fetchTarball https://github.com/nh2/static-haskell-nix/archive/d1b20f35ec7d3761e59bd323bbe0cca23b3dfc82.tar.gz;
 
   # Pin nixpkgs version
   # By default to the one `static-haskell-nix` provides, but you may also give
@@ -20,7 +20,7 @@ let
   stack2nix-script = import "${static-haskell-nix}/static-stack2nix-builder/stack2nix-script.nix" {
     inherit pkgs;
     stack-project-dir = toString ./.; # where stack.yaml is
-    hackageSnapshot = "2019-11-20T00:00:00Z"; # pins e.g. extra-deps without hashes or revisions
+    hackageSnapshot = "2020-03-20T00:00:00Z"; # pins e.g. extra-deps without hashes or revisions
   };
 
   periodicd-builder = import "${static-haskell-nix}/static-stack2nix-builder/default.nix" {
