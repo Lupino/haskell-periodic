ff50031969ae551aaa84f2cc9bb038b24468d10c
Lupino <lmjubuntu@gmail.com>
2021-05-24 15:43:58 +0800

    update nix files

--- nix/overlays/haskell-packages/default.nix
+++ nix/overlays/haskell-packages/default.nix
@@ -2,45 +2,21 @@
 
 self: super:
 let
+  metroRepo = super.fetchFromGitHub {
+    owner = "Lupino";
+    repo = "metro";
+    rev = "84fc5a4b4513cdb8bad6df2182097bcc7bc12f93";
+    sha256 = "0iw0vlzl98fc9iq4p893v1k3a1d86cpyvdzmqmkva5dgry6s8n3n";
+  };
   overrides =
     final: prev:
     rec {
-      metro = prev.callHackageDirect
-        {
-          pkg = "metro";
-          ver = "0.1.0.5";
-          sha256 = "0sc8wrckl0i08s8b7j3b7fpfv98bxh65wzlg3bkz0klwmws0kv4w";
-        } { };
-      metro-socket = prev.callHackageDirect
-        {
-          pkg = "metro-socket";
-          ver = "0.1.0.1";
-          sha256 = "0qzfrldhmwli288cy4ls8w5hyr17zsv63pcfjgwpqkqld5cybflw";
-        } { };
-      metro-transport-tls = prev.callHackageDirect
-        {
-          pkg = "metro-transport-tls";
-          ver = "0.1.0.0";
-          sha256 = "1prx6005m8422i5n601vdhbl7c2wygds6va5lq893rqdb364i26m";
-        } { };
-      metro-transport-websockets = prev.callHackageDirect
-        {
-          pkg = "metro-transport-websockets";
-          ver = "0.1.0.0";
-          sha256 = "0b5hg74lpyihn7516a9ja3jbrrwhh1fhidvi39zn1hrh50jhbh43";
-        } { };
-      metro-transport-xor = prev.callHackageDirect
-        {
-          pkg = "metro-transport-xor";
-          ver = "0.1.0.0";
-          sha256 = "1hl07x98sl6vx7xyf8wbsa2km37zngdpbrj1g0lg8bp9rk91fcmr";
-        } { };
-      hashmap-io = prev.callHackageDirect
-        {
-          pkg = "hashmap-io";
-          ver = "0.1.0.0";
-          sha256 = "0kzwqjwwdwcfghyq3rakg3yqrh0aa2mgh98cknnsqf7ccd08667l";
-        } { };
+      metro = prev.callCabal2nix "metro" (metroRepo) {};
+      metro-socket = prev.callCabal2nix "metro-socket" ((metroRepo) + /metro-socket) {};
+      metro-transport-tls = prev.callCabal2nix "metro-transport-tls" ((metroRepo) + /metro-transport-tls) {};
+      metro-transport-websockets = prev.callCabal2nix "metro-transport-websockets" ((metroRepo) + /metro-transport-websockets) {};
+      metro-transport-xor = prev.callCabal2nix "metro-transport-xor" ((metroRepo) + /metro-transport-xor) {};
+      map-io = prev.callCabal2nix "map-io" ((metroRepo) + /map-io) {};
     } // extraOverrides final prev;
 in
 {
