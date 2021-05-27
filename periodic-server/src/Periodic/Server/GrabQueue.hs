0cf9449610c337b817d9a7010efca40b1e427212
Lupino <lmjubuntu@gmail.com>
2021-05-27 16:12:07 +0800

    periodic-serveer: update GrabQueue

--- periodic-server/src/Periodic/Server/GrabQueue.hs
+++ periodic-server/src/Periodic/Server/GrabQueue.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TupleSections #-}
+
 module Periodic.Server.GrabQueue
   ( GrabQueue
   , newGrabQueue
@@ -8,38 +10,56 @@ module Periodic.Server.GrabQueue
 
 
 import           Data.ByteString (ByteString)
-import           Data.IOMap      (IOMap)
-import qualified Data.IOMap      as IOMap
-import qualified Data.IOMap.STM  as IOMapS
 import           Data.List       (nub)
 import           Periodic.Types  (FuncName)
-import           UnliftIO        (MonadIO, STM, retrySTM)
+import           UnliftIO        (MonadIO, STM, TVar, atomically, newTVarIO,
+                                  readTVar, retrySTM, writeTVar)
 
-newtype GrabQueue = GrabQueue (IOMap FuncName [ByteString])
+newtype GrabQueue = GrabQueue (TVar [(FuncName, ByteString)])
 
 newGrabQueue :: MonadIO m => m GrabQueue
-newGrabQueue = GrabQueue <$> IOMap.empty
+newGrabQueue = GrabQueue <$> newTVarIO []
 
 pushAgent :: MonadIO m => GrabQueue -> [FuncName] -> ByteString -> m ()
-pushAgent (GrabQueue q) funcList k =
-  mapM_ (\fn -> IOMap.insertLookupWithKey f fn [k] q) funcList
-  where f _ nv ov = nub $ nv ++ ov
+pushAgent (GrabQueue q) funcList k = atomically $ do
+  ov <- readTVar q
+  writeTVar q $! nub $ ov ++ map (,k) funcList
 
 popAgentSTM :: GrabQueue -> FuncName -> STM ByteString
-popAgentSTM (GrabQueue q) fn = do
-  ks <- IOMapS.insertLookupWithKey f fn [] q
-  case ks of
-    Nothing    -> retrySTM
-    Just []    -> retrySTM
-    Just (x:_) -> return x
+popAgentSTM (GrabQueue q) fn0 = do
+  agents <- readTVar q
+  case getOne agents of
+    Nothing -> retrySTM
+    Just agent -> do
+      writeTVar q $! removeAgent agent agents
+      return agent
+  where getOne :: [(FuncName, ByteString)] -> Maybe ByteString
+        getOne [] = Nothing
+        getOne ((fn1, agent) : xs)
+          | fn1 == fn0 = Just agent
+          | otherwise = getOne xs
 
-  where f _ _ []     = []
-        f _ _ (_:xs) = xs
+        removeAgent :: ByteString -> [(FuncName, ByteString)] -> [(FuncName, ByteString)]
+        removeAgent _ [] = []
+        removeAgent agent0 ((fn1, agent1) : xs)
+          | agent0 == agent1 = removeAgent agent0 xs
+          | otherwise = (fn1, agent1) : removeAgent agent0 xs
+
 
 popAgentList :: MonadIO m => GrabQueue -> FuncName -> m [ByteString]
-popAgentList (GrabQueue q) fn = do
-  ks <- IOMap.insertLookupWithKey f fn [] q
-  case ks of
-    Nothing -> pure []
-    Just xs -> pure xs
-  where f _ _ _ = []
+popAgentList (GrabQueue q) fn0 = atomically $ do
+  agents <- readTVar q
+  let agentList = getAll agents
+  writeTVar q $! removeAgent agentList agents
+  return agentList
+  where getAll :: [(FuncName, ByteString)] -> [ByteString]
+        getAll [] = []
+        getAll ((fn1, agent) : xs)
+          | fn1 == fn0 = agent : getAll xs
+          | otherwise = getAll xs
+
+        removeAgent :: [ByteString] -> [(FuncName, ByteString)] -> [(FuncName, ByteString)]
+        removeAgent _ [] = []
+        removeAgent agents ((fn1, agent1) : xs)
+          | agent1 `elem` agents = removeAgent agents xs
+          | otherwise = (fn1, agent1) : removeAgent agents xs
