4766d98827304ebb584748f1db1312c92be8f372
Lupino <lmjubuntu@gmail.com>
2021-05-27 19:22:28 +0800

    periodic-server: reimplement GrabItem

--- periodic-server/src/Periodic/Server/GrabQueue.hs
+++ periodic-server/src/Periodic/Server/GrabQueue.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections #-}
-
 module Periodic.Server.GrabQueue
   ( GrabQueue
   , newGrabQueue
@@ -14,51 +12,49 @@ import           Periodic.Types (FuncName, Msgid, Nid)
 import           UnliftIO       (MonadIO, STM, TVar, atomically, newTVarIO,
                                  readTVar, retrySTM, writeTVar)
 
-newtype GrabQueue = GrabQueue (TVar [(FuncName, (Nid, Msgid))])
+data GrabItem = GrabItem
+  { gNid      :: Nid
+  , gMsgid    :: Msgid
+  , gFuncList :: [FuncName]
+  }
+
+instance Eq GrabItem where
+  a == b = gNid a == gNid b && gMsgid a == gMsgid b
+
+newtype GrabQueue = GrabQueue (TVar [GrabItem])
 
 newGrabQueue :: MonadIO m => m GrabQueue
 newGrabQueue = GrabQueue <$> newTVarIO []
 
 pushAgent :: MonadIO m => GrabQueue -> [FuncName] -> (Nid, Msgid) -> m ()
-pushAgent (GrabQueue q) funcList agent = atomically $ do
+pushAgent (GrabQueue q) funcList (nid, msgid) = atomically $ do
   ov <- readTVar q
-  writeTVar q $! nub $ ov ++ map (, agent) funcList
+  writeTVar q $! nub $ item : ov
+  where item = GrabItem nid msgid funcList
 
 popAgentSTM :: GrabQueue -> FuncName -> STM (Nid, Msgid)
 popAgentSTM (GrabQueue q) fn0 = do
-  agents <- readTVar q
-  case getOne agents of
+  (magent, items) <- lookupAndRemove [] <$> readTVar q
+  case magent of
     Nothing -> retrySTM
     Just agent -> do
-      writeTVar q $! removeAgent agent agents
+      writeTVar q $! items
       return agent
-  where getOne :: [(FuncName, (Nid, Msgid))] -> Maybe (Nid, Msgid)
-        getOne [] = Nothing
-        getOne ((fn1, agent) : xs)
-          | fn1 == fn0 = Just agent
-          | otherwise = getOne xs
 
-        removeAgent :: (Nid, Msgid) -> [(FuncName, (Nid, Msgid))] -> [(FuncName, (Nid, Msgid))]
-        removeAgent _ [] = []
-        removeAgent agent0 ((fn1, agent1) : xs)
-          | agent0 == agent1 = removeAgent agent0 xs
-          | otherwise = (fn1, agent1) : removeAgent agent0 xs
+  where lookupAndRemove :: [GrabItem] -> [GrabItem] -> (Maybe (Nid, Msgid), [GrabItem])
+        lookupAndRemove prev [] = (Nothing, prev)
+        lookupAndRemove prev (x:xs)
+          | fn0 `elem` gFuncList x = (Just (gNid x, gMsgid x), prev ++ xs)
+          | otherwise = lookupAndRemove (prev ++ [x]) xs
 
 
 popAgentList :: MonadIO m => GrabQueue -> FuncName -> m [(Nid, Msgid)]
 popAgentList (GrabQueue q) fn0 = atomically $ do
-  agents <- readTVar q
-  let agentList = getAll agents
-  writeTVar q $! removeAgent agentList agents
-  return agentList
-  where getAll :: [(FuncName, (Nid, Msgid))] -> [(Nid, Msgid)]
-        getAll [] = []
-        getAll ((fn1, agent) : xs)
-          | fn1 == fn0 = agent : getAll xs
-          | otherwise = getAll xs
-
-        removeAgent :: [(Nid, Msgid)] -> [(FuncName, (Nid, Msgid))] -> [(FuncName, (Nid, Msgid))]
-        removeAgent _ [] = []
-        removeAgent agents ((fn1, agent1) : xs)
-          | agent1 `elem` agents = removeAgent agents xs
-          | otherwise = (fn1, agent1) : removeAgent agents xs
+  (agents, items) <- lookupAndRemove [] [] <$> readTVar q
+  writeTVar q $! items
+  return agents
+  where lookupAndRemove :: [(Nid, Msgid)] -> [GrabItem] -> [GrabItem] -> ([(Nid, Msgid)], [GrabItem])
+        lookupAndRemove agents prev [] = (agents, prev)
+        lookupAndRemove agents prev (x:xs)
+          | fn0 `elem` gFuncList x = lookupAndRemove (agents ++ [(gNid x, gMsgid x)]) prev xs
+          | otherwise = lookupAndRemove agents (prev ++ [x]) xs
