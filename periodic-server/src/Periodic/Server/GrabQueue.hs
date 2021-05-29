9a2619bcfc005e435414acfd93c0e005bd711065
Lupino <lmjubuntu@gmail.com>
2021-05-29 08:49:26 +0800

    periodic-server: GrabQueue use IOMap with Nid index

--- periodic-server/src/Periodic/Server/GrabQueue.hs
+++ periodic-server/src/Periodic/Server/GrabQueue.hs
@@ -7,60 +7,63 @@ module Periodic.Server.GrabQueue
   ) where
 
 
-import           Control.Monad  (unless)
+import           Data.IOMap     (IOMap)
+import qualified Data.IOMap     as IOMap
+import qualified Data.IOMap.STM as IOMapS
+import           Data.Maybe     (catMaybes)
 import           Periodic.Types (FuncName, Msgid, Nid)
-import           UnliftIO       (MonadIO, STM, TVar, atomically, newTVarIO,
-                                 readTVar, retrySTM, writeTVar)
+import           UnliftIO       (MonadIO, STM, TVar, atomically, readTVar,
+                                 retrySTM)
 
 data GrabItem = GrabItem
-  { gNid      :: !Nid
-  , gMsgid    :: !Msgid
-  , gFuncList :: TVar [FuncName]
+  { msgidList :: ![Msgid]
+  , funcList  :: TVar [FuncName]
   }
 
-instance Eq GrabItem where
-  a == b = gNid a == gNid b && gMsgid a == gMsgid b
-
-newtype GrabQueue = GrabQueue (TVar [GrabItem])
+newtype GrabQueue = GrabQueue (IOMap Nid GrabItem)
 
 newGrabQueue :: MonadIO m => m GrabQueue
-newGrabQueue = GrabQueue <$> newTVarIO []
+newGrabQueue = GrabQueue <$> IOMap.empty
 
 pushAgent :: MonadIO m => GrabQueue -> TVar [FuncName] -> Nid -> Msgid -> m ()
-pushAgent (GrabQueue q) funcList nid msgid = atomically $ do
-  ov <- readTVar q
-  unless (item `elem` ov) $ writeTVar q $! ov ++ [item]
-  where item = GrabItem nid msgid funcList
+pushAgent (GrabQueue q) fl nid msgid =
+  IOMap.alter alterFunc nid q
+  where alterFunc :: Maybe GrabItem -> Maybe GrabItem
+        alterFunc Nothing = Just $ GrabItem [msgid] fl
+        alterFunc (Just item)
+          | msgid `elem` msgidList item = Just item
+          | otherwise = Just item {msgidList = msgidList item ++ [msgid]}
+
+findMsgid :: GrabQueue -> FuncName -> Nid -> STM (Maybe (Nid, Msgid))
+findMsgid (GrabQueue q) fn nid = do
+  mitem <- IOMapS.lookup nid q
+  case mitem of
+    Nothing -> pure Nothing
+    Just GrabItem{msgidList = []} -> pure Nothing
+    Just item@GrabItem{msgidList=(x:xs)} -> do
+      funcs <- readTVar $ funcList item
+      if fn `elem` funcs then do
+        IOMapS.insert nid item {msgidList = xs} q
+        pure $ Just (nid, x)
+        else pure Nothing
 
 popAgent :: MonadIO m => GrabQueue -> FuncName -> m (Nid, Msgid)
-popAgent (GrabQueue q) fn = atomically $ do
-  (mmsgid, items) <- lookupAndRemove [] =<< readTVar q
-  case mmsgid of
+popAgent gq@(GrabQueue q) fn = atomically $ do
+  nids <- IOMapS.keys q
+  r <- go nids
+  case r of
     Nothing -> retrySTM
-    Just item -> do
-      writeTVar q $! items
-      return (gNid item, gMsgid item)
+    Just rr -> pure rr
+  where go :: [Nid] -> STM (Maybe (Nid, Msgid))
+        go [] = pure Nothing
+        go (x:xs) = do
+          r <- findMsgid gq fn x
+          case r of
+            Nothing -> go xs
+            Just rr -> pure $ Just rr
 
-  where lookupAndRemove :: [GrabItem] -> [GrabItem] -> STM (Maybe GrabItem, [GrabItem])
-        lookupAndRemove prev [] = pure (Nothing, prev)
-        lookupAndRemove prev (x:xs) = do
-          funcList <- readTVar $ gFuncList x
-          if fn `elem` funcList then pure (Just x, prev ++ xs)
-                                else lookupAndRemove (prev ++ [x]) xs
 
 popAgentList :: MonadIO m => GrabQueue -> FuncName -> m [(Nid, Msgid)]
-popAgentList (GrabQueue q) fn = atomically $ do
-  (agents, items) <- lookupAndRemove [] [] [] =<< readTVar q
-  writeTVar q $! items
-  return [(gNid item, gMsgid item) | item <- agents]
-
-  where lookupAndRemove
-          :: [Nid] -> [GrabItem]
-          -> [GrabItem] -> [GrabItem]
-          -> STM ([GrabItem], [GrabItem])
-        lookupAndRemove _ got prev [] = pure (got, prev)
-        lookupAndRemove nids got prev (x:xs) = do
-          funcList <- readTVar $ gFuncList x
-          if fn `elem` funcList && gNid x `notElem` nids
-             then lookupAndRemove (nids ++ [gNid x]) (got ++ [x]) prev xs
-             else lookupAndRemove nids got (prev ++ [x]) xs
+popAgentList gq@(GrabQueue q) fn = atomically $ do
+  nids <- IOMapS.keys q
+  catMaybes <$> mapM (findMsgid gq fn) nids
