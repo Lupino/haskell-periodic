7f75e1ea04240efe56fa005e43dca77e6fad7f78
Lupino <lmjubuntu@gmail.com>
2024-07-03 10:29:27 +0800

    update sha256map

--- nix/sha256map.nix
+++ nix/sha256map.nix
@@ -1,4 +1,4 @@
 {
   "https://github.com/Lupino/metro.git"."01e84ebb4a4a1b1872cfacee7179b5e72d23b0e4" = "1z2xwlxry19c7fgfjnj5l1dwmfdr1snm8m4cjas8pvm3bz78h7cx";
-  "https://github.com/Lupino/yuntan-common.git"."eafbf3326065ab7b1df63f3fdd4f6c4f2d1f22b7" = "1wn5phfpvfsariv57a00k7jh5knglmx2azv4wdx4001p1hwsh9aw";
+  "https://github.com/Lupino/yuntan-common.git"."1b751c753d684bd654907f337a9ac6560e7889b3" = "0vg0iaa8nzzqgji69g92ykl01szz0xp2k52ggiivz1rlk033llvn";
 }
