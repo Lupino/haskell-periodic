4e84b0928bcc8672bd3124de2361b0e6595fdc36
Lupino <lmjubuntu@gmail.com>
2025-12-28 10:01:20 +0800

    set version 1.2.4.0

--- nix/sha256map.nix
+++ nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/Lupino/metro.git"."6518b7e66e6f5acf6a6c8d28bed6ebcd184a73d5" = "034vlgzh2hi31rnbvhnc1flc3h05f5qrp08w5d65hkivbxyc7ias";
+  "https://github.com/Lupino/metro.git"."e16a707cfabd6cc73da3c351688aad77db255b08" = "0ks0gy72hkc4lpyfj9r9syf0qm03xmn0iqn99g5sisdhgd23f9s6";
   "https://github.com/Lupino/yuntan-common.git"."dd5b6bc3cc5bba153b249f92565b61dfe073d048" = "1cak7gf1qlkfz60bs0nbkl8d1vvzi2dwska6n2alhw1qp54pk93x";
   "https://github.com/buecking/hs-mcp.git"."6df508fe1b1f3f62d15e0b012233fa7536f8fceb" = "034grcmm6i2xa9gm7l42vvg0wl8i1wqmy1h3mr3nl66yrrm3a615";
 }
