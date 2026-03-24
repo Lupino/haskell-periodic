61aba6214aa1eb0f12f9ff7b7ad0576dca76a54e
Lupino <lmjubuntu@gmail.com>
2026-03-24 09:28:27 +0800

    chore(deps): update yuntan-common revision and refresh freeze lockfile

--- nix/sha256map.nix
+++ nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
   "https://github.com/Lupino/metro.git"."2521ea32857e5ff6ff2d9a99b1ddacac35903c88" = "1kay1syr09ifkcy5s05gihikmfd6s8nk9h50nlwip5jivs88n9i7";
-  "https://github.com/Lupino/yuntan-common.git"."45d09076d78602276d85ec97bd5ad26ad4f2855f" = "159f1z8p0wv4vxi3xj0ba7pz0xfj90dr7l06ba06qb3cj6d3kz5r";
+  "https://github.com/Lupino/yuntan-common.git"."3ca2761a7653b81a38a8669aedcbe790219391ec" = "1cnx3cklmavnnapv7nzfhiqr0whpmsqi9ib2jf2dmy7mmnwk355a";
   "https://github.com/buecking/hs-mcp.git"."6df508fe1b1f3f62d15e0b012233fa7536f8fceb" = "034grcmm6i2xa9gm7l42vvg0wl8i1wqmy1h3mr3nl66yrrm3a615";
 }
