a1a17165086e3a0c6513dc125dbdd98d473465cc
Lupino <lmjubuntu@gmail.com>
2026-03-26 10:17:24 +0800

    chore: bump metro to latest pinned commit

--- nix/sha256map.nix
+++ nix/sha256map.nix
@@ -1,5 +1,5 @@
 {
-  "https://github.com/Lupino/metro.git"."fc8be1d238e0a8eb75393cc0627f466be89ada93" = "0mp91xwrv5ifh4nd729jffsissmdygrn2wv4s8zpz8q6zhhfpx69";
+  "https://github.com/Lupino/metro.git"."c9c5e1c109e23116c8405d7371c43af7d50fd888" = "0gy0wimdq1fr89bd342ydr8v1qa3xahq8ykxgn5qc9883pjd6c02";
   "https://github.com/Lupino/yuntan-common.git"."235bd76d53e8d7946d8aa29beccc4ba0d825ec82" = "17rxh5z4139as2m81jxw3mmshjlfymia1j7q09gikx1pa65ybqhm";
   "https://github.com/buecking/hs-mcp.git"."6df508fe1b1f3f62d15e0b012233fa7536f8fceb" = "034grcmm6i2xa9gm7l42vvg0wl8i1wqmy1h3mr3nl66yrrm3a615";
 }
